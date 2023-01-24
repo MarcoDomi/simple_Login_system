b3570e5efb68e945868f5553bc4f993bb720f93d
MarcoDominguez <marcod082@gmail.com>
2023-01-24 02:40:29 -0600

    finished login function

--- main.cpp
+++ main.cpp
@@ -4,9 +4,41 @@
 
 
 void login(){
-    
+    std::string username;
+    std::string password;
 
-   
+    std::cout << "Enter username: ";
+    getline(std::cin, username);
+
+    std::cout << "Enter password: ";
+    std::cin >> password;
+
+    username.append(".txt");
+
+    std::ifstream userFile;
+
+    userFile.open(username);
+
+    bool username_check = false;
+    bool password_check = false;
+
+    if(userFile.is_open()){
+        username_check = true;
+
+        std::string pw_inFile;
+
+        userFile >> pw_inFile;
+
+        if(password == pw_inFile)
+            password_check = true;
+    }
+
+    bool login_status = username_check && password_check;
+
+    if(login_status)
+        std::cout << "Login Successful!" << std::endl;
+    else
+        std::cout << "Incorrect Username or Password" << std::endl;
 }
 
 //TODO: IMPLEMENT INPUT VALIDATION
